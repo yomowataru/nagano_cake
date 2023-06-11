643457bb0004903a46743d928d3c736aaebbd6a7
EC2 Default User <yomoyomo0503@gmail.com>
2023-06-11 07:04:43 +0000

    [Add]devise以外のコントローラー作成

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,16 @@
+class Public::OrdersController < ApplicationController
+  def new
+  end
+
+  def comfirm
+  end
+
+  def complete
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,56 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/comfirm'
+    get 'orders/complete'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/destroy'
+    get 'cart_items/destroy_all'
+    get 'cart_items/create'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/unsubscribe'
+    get 'customers/withdraw'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions" 
+    sessions: "admin/sessions"
   }
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
-  } 
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
