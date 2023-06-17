077cd7a89e265b7d31f7e562d1ba00f7e48bcfd5
EC2 Default User <yomoyomo0503@gmail.com>
2023-06-17 07:18:59 +0000

    [Add]管理者:会員情報編集画面作成(会員ステータス変更除く)

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,6 +8,10 @@ class Customer < ApplicationRecord
     self.last_name + self.first_name
   end
   
+  def full_name_kana
+    self.last_name_kana + self.first_name_kana 
+  end
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
