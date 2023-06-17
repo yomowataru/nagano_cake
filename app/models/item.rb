accc6717dd5318a164f9a7f98242b2b589d634a8
EC2 Default User <yomoyomo0503@gmail.com>
2023-06-17 03:06:39 +0000

    [Add]顧客:詳細画面作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
   has_one_attached :image
   
-  def get_image(width, height)
+  # (width,height)
+  def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg') 
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
