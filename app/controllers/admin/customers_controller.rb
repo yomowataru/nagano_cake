7918aa7874c5a4f8fd294713932dff6f16d32eef
EC2 Default User <yomoyomo0503@gmail.com>
2023-06-25 05:08:55 +0000

    [Add]管理者:会員編集(ステータス変更)機能作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,19 +4,28 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
-    @customer = Customer.find(params[:id]) 
+    @customer = Customer.find(params[:id])
   end
 
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     customer =  Customer.find(params[:id])
-    customer.update(customer_params)  
+    customer.update(customer_params)
+
+    if params[:customer][:status] == "1"
+      customer.update(is_deleted: true)
+      flash[:notice] = "退会処理を実行いたしました"
+
+    elsif params[:customer][:status] == "0"
+      customer.update(is_deleted: false)
+    end
+
     redirect_to admin_customer_path(customer.id)
   end
-  
+
   def customer_params
     params.require(:customer).permit(:email,:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
   end
